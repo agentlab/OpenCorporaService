7dd25008d48b3365c22d5abd533b56766ac3d6b0
DemetrumYats <naymushin_dmitry@mail.ru>
2017-02-21 14:30:41 +0300

    reformat

--- plugins/com.bmstu.coursework.opencopra.ds.host/src/com/bmstu/coursework/opencopra/ds/host/OpenCopraComponent.java
+++ plugins/com.bmstu.coursework.opencopra.ds.host/src/com/bmstu/coursework/opencopra/ds/host/OpenCopraComponent.java
@@ -43,30 +43,29 @@ public class OpenCopraComponent
     public Response post(String service, String action, String section, String name, String left, String right,
         String id, List<String> params) {
 
-
         switch (section) {
-		case "grammemes": //$NON-NLS-1$
+        case "grammemes": //$NON-NLS-1$
             GrammemeProcessor gmp = new GrammemeProcessor();
             return Response.ok().entity(gmp.create(name, params)).build();
 
-		case "restrictions": //$NON-NLS-1$
+        case "restrictions": //$NON-NLS-1$
             RestrictionProcessor rtp = new RestrictionProcessor();
             return Response.ok().entity(rtp.create(left, right, params)).build();
 
-		case "lemmata": //$NON-NLS-1$
+        case "lemmata": //$NON-NLS-1$
             LemmaProcessor lmp = new LemmaProcessor();
             return Response.ok().entity(lmp.create(id, params)).build();
 
-		case "link_types": //$NON-NLS-1$
+        case "link_types": //$NON-NLS-1$
             LinkTypeProcessor ltp = new LinkTypeProcessor();
             return Response.ok().entity(ltp.create(id, params)).build();
 
-		case "links": //$NON-NLS-1$
+        case "links": //$NON-NLS-1$
             LinkProcessor lkp = new LinkProcessor();
             return Response.ok().entity(lkp.create(id, params)).build();
 
         default:
-			return Response.ok().entity("Error: wrong section name!").build(); //$NON-NLS-1$
+            return Response.ok().entity("Error: wrong section name!").build(); //$NON-NLS-1$
         }
     }
 
@@ -75,29 +74,28 @@ public class OpenCopraComponent
     public Response get(String service, String action, String section, String name, String left, String right,
         String id) {
 
-
         switch (section) {
-		case "grammemes": //$NON-NLS-1$
+        case "grammemes": //$NON-NLS-1$
             GrammemeProcessor gmp = new GrammemeProcessor();
             return Response.ok().entity(gmp.read(name)).build();
 
-		case "restrictions": //$NON-NLS-1$
+        case "restrictions": //$NON-NLS-1$
             RestrictionProcessor rtp = new RestrictionProcessor();
             return Response.ok().entity(rtp.read(left, right)).build();
-		case "lemmata": //$NON-NLS-1$
+        case "lemmata": //$NON-NLS-1$
             LemmaProcessor lmp = new LemmaProcessor();
             return Response.ok().entity(lmp.read(id)).build();
 
-		case "link_types": //$NON-NLS-1$
+        case "link_types": //$NON-NLS-1$
             LinkTypeProcessor ltp = new LinkTypeProcessor();
             return Response.ok().entity(ltp.read(id)).build();
 
-		case "links": //$NON-NLS-1$
+        case "links": //$NON-NLS-1$
             LinkProcessor lkp = new LinkProcessor();
             return Response.ok().entity(lkp.read(id)).build();
 
         default:
-			return Response.ok().entity("Error: wrong section name!").build(); //$NON-NLS-1$
+            return Response.ok().entity("Error: wrong section name!").build(); //$NON-NLS-1$
         }
     }
 
@@ -106,30 +104,29 @@ public class OpenCopraComponent
     public Response put(String service, String action, String section, String name, String left, String right,
         String id, List<String> params) {
 
-
         switch (section) {
-		case "grammemes": //$NON-NLS-1$
+        case "grammemes": //$NON-NLS-1$
             GrammemeProcessor gmp = new GrammemeProcessor();
             return Response.ok().entity(gmp.update(name, params)).build();
 
-		case "restrictions": //$NON-NLS-1$
+        case "restrictions": //$NON-NLS-1$
             RestrictionProcessor rtp = new RestrictionProcessor();
             return Response.ok().entity(rtp.update(left, right, params)).build();
 
-		case "lemmata": //$NON-NLS-1$
+        case "lemmata": //$NON-NLS-1$
             LemmaProcessor lmp = new LemmaProcessor();
             return Response.ok().entity(lmp.update(id, params)).build();
 
-		case "link_types": //$NON-NLS-1$
+        case "link_types": //$NON-NLS-1$
             LinkTypeProcessor ltp = new LinkTypeProcessor();
             return Response.ok().entity(ltp.update(id, params)).build();
 
-		case "links": //$NON-NLS-1$
+        case "links": //$NON-NLS-1$
             LinkProcessor lkp = new LinkProcessor();
             return Response.ok().entity(lkp.update(id, params)).build();
 
         default:
-			return Response.ok().entity("Error: wrong section name!").build(); //$NON-NLS-1$
+            return Response.ok().entity("Error: wrong section name!").build(); //$NON-NLS-1$
         }
     }
 
@@ -139,28 +136,28 @@ public class OpenCopraComponent
         String id) {
 
         switch (section) {
-		case "grammemes": //$NON-NLS-1$
+        case "grammemes": //$NON-NLS-1$
             GrammemeProcessor gmp = new GrammemeProcessor();
             return Response.ok().entity(gmp.delete(name)).build();
 
-		case "restrictions": //$NON-NLS-1$
+        case "restrictions": //$NON-NLS-1$
             RestrictionProcessor rtp = new RestrictionProcessor();
             return Response.ok().entity(rtp.delete(left, right)).build();
 
-		case "lemmata": //$NON-NLS-1$
+        case "lemmata": //$NON-NLS-1$
             LemmaProcessor lmp = new LemmaProcessor();
             return Response.ok().entity(lmp.delete(id)).build();
 
-		case "link_types": //$NON-NLS-1$
+        case "link_types": //$NON-NLS-1$
             LinkTypeProcessor ltp = new LinkTypeProcessor();
             return Response.ok().entity(ltp.delete(id)).build();
 
-		case "links": //$NON-NLS-1$
+        case "links": //$NON-NLS-1$
             LinkProcessor lkp = new LinkProcessor();
             return Response.ok().entity(lkp.delete(id)).build();
 
         default:
-			return Response.ok().entity("Error: wrong section name!").build(); //$NON-NLS-1$
+            return Response.ok().entity("Error: wrong section name!").build(); //$NON-NLS-1$
         }
     }
 
