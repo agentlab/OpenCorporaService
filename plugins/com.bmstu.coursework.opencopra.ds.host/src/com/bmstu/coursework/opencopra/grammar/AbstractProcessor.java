077ddb6f06471874ed3b4a3a0f012bc774499131
DmitryZagr <bikemaster2000@list.ru>
2017-02-21 12:14:29 +0300

    refactor resources path

--- plugins/com.bmstu.coursework.opencopra.ds.host/src/com/bmstu/coursework/opencopra/grammar/AbstractProcessor.java
+++ plugins/com.bmstu.coursework.opencopra.ds.host/src/com/bmstu/coursework/opencopra/grammar/AbstractProcessor.java
@@ -12,7 +12,7 @@ import com.bmstu.coursework.opencopra.util.OpenCopraUtil;
 public abstract class AbstractProcessor {
 
     protected static final String OLD_PATH =
-        OpenCopraUtil.getPluginDir("com.bmstu.coursework.opencopra.resources") + "xml/dict.xml"; //$NON-NLS-1$ //$NON-NLS-2$
+        OpenCopraUtil.getPluginDir("com.bmstu.coursework.opencopra.resources") + "src/main/resources/xml/dict.xml"; //$NON-NLS-1$ //$NON-NLS-2$
     public static final String NEW_PATH =
-        OpenCopraUtil.getPluginDir("com.bmstu.coursework.opencopra.resources") + "xml/dict_new.xml"; //$NON-NLS-1$ //$NON-NLS-2$
+        OpenCopraUtil.getPluginDir("com.bmstu.coursework.opencopra.resources") + "src/main/resources/xml/dict_new.xml"; //$NON-NLS-1$ //$NON-NLS-2$
 }
